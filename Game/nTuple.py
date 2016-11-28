9e68b74673c39a035132357f59bc25c6a3149678
Jeremy Abel <jabel415@gmail.com>
2016-11-28 12:49:39 -0500

    Pushing previous changes to nTuple that were intially kept locally

--- Game/nTuple.py
+++ Game/nTuple.py
@@ -22,20 +22,34 @@ class Ntuple():
         weights = random.uniform(minWeight, maxWeight, len(weights))
         return Ntuple(numValues,locations,weights)
 
+    def valueAtLocation(self, board):
+        return self.lookUpTable[self.tileAddress(board)]
+
     # compute new weight for move
     @staticmethod
     def computeNewWeights(numValues, numFields):
         return (math.pow(numValues, numFields))# + 0.5)
 
-    def tileAddress(self, replaceWithBoardObject):
+    def tileAddress(self, board):
         address = 0
         for x in self.locations:
             address *= self.numValues
-            #address += board.getVal(x)
+            address += board.getVal(x)
+        return address
+
+    #def valueFromAddress(self, address):
+    #    val = []*sys.getSizeOf(val)
+
+    def getNumWeights(self):
+        return len(self.lookUpTable)
+
+    def getNumVals(self):
+        return self.numValues
 
 
     # str test function
     def __str__(self):
         return "Ntuple(" + str(self.numValues) + "," + str(self.locations) + "," + str(self.lookUpTable) + ")"
 
+#tests of tuple creation
 ntuple = Ntuple(5, [(0,1),(2,1)],[1,1,1])
