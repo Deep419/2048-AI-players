3e09f197f25091f7e1a9fcdab904db80f4e2f2a0
Deep Ghaghara <dghaghara@gmail.com>
2019-01-26 18:09:48 -0500

    yapf formatted. Updated gitignore

--- Game/logic.py
+++ Game/logic.py
@@ -1,13 +1,6 @@
 from random import *
 
-#######
-#Task 1a#
-#######
 
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 1 mark for creating the correct matrix
 def new_game(n):
     matrix = []
 
@@ -15,204 +8,170 @@ def new_game(n):
         matrix.append([0] * n)
     return matrix
 
-###########
-# Task 1b #
-###########
 
-# [Marking Scheme]
-# Points to note:
-# Must ensure that it is created on a zero entry
-# 1 mark for creating the correct loop
-# updated so chances of 2 and 4 being added are correct - Nico Gonzalez
+# updated so chances of 2 and 4 being added are correct
 def add_two(mat):
-    a=randint(0,len(mat)-1)
-    b=randint(0,len(mat)-1)
-    num=0
+    a = randint(0, len(mat) - 1)
+    b = randint(0, len(mat) - 1)
     if randint(0, 99) < 100 * 0.9:
-        num= 2
+        num = 2
     else:
-        num= 4
-    while(mat[a][b]!=0):
-        a=randint(0,len(mat)-1)
-        b=randint(0,len(mat)-1)
+        num = 4
+    while mat[a][b] != 0:
+        a = randint(0, len(mat) - 1)
+        b = randint(0, len(mat) - 1)
     mat[a][b] = num
     return mat
 
-###########
-# Task 1c #
-###########
 
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 0 marks for completely wrong solutions
-# 1 mark for getting only one condition correct
-# 2 marks for getting two of the three conditions
-# 3 marks for correct checking
-
-##Find max tile value
-##Author: Eva Yang
+# Find max tile value
 def max_tile(mat):
-    max=0
+    max_value = 0
     for rows in mat:
         row = rows
         for tile in range(len(row)):
-            if row[tile]>max:
-                max=row[tile]
-    return max
+            if row[tile] > max_value:
+                max_value = row[tile]
+    return max_value
 
-##Find score
-##Author: Eva Yang
+
+# Find score
 def score(mat):
-    score=0
+    score_value = 0
     for rows in mat:
         row = rows
         for tile in range(len(row)):
-            score=score+row[tile]
-    return score
-
+            score_value = score_value + row[tile]
+    return score_value
 
 
 def game_state(mat):
     for i in range(len(mat)):
         for j in range(len(mat[0])):
-            if mat[i][j]==2048:
+            if mat[i][j] == 2048:
                 return 'win'
-    for i in range(len(mat)-1): #intentionally reduced to check the row on the right and below
-        for j in range(len(mat[0])-1): #more elegant to use exceptions but most likely this will be their solution
-            if mat[i][j]==mat[i+1][j] or mat[i][j+1]==mat[i][j]:
+    # intentionally reduced to check the row on the right and below
+    for i in range(len(mat) - 1):
+        # more elegant to use exceptions but most likely this will be their solution
+        for j in range(len(mat[0]) - 1):
+            if mat[i][j] == mat[i + 1][j] or mat[i][j + 1] == mat[i][j]:
                 return 'not over'
-    for i in range(len(mat)): #check for any zero entries
+    # check for any zero entries
+    for i in range(len(mat)):
         for j in range(len(mat[0])):
-            if mat[i][j]==0:
+            if mat[i][j] == 0:
                 return 'not over'
-    for k in range(len(mat)-1): #to check the left/right entries on the last row
-        if mat[len(mat)-1][k]==mat[len(mat)-1][k+1]:
+    # to check the left/right entries on the last row
+    for k in range(len(mat) - 1):
+        if mat[len(mat) - 1][k] == mat[len(mat) - 1][k + 1]:
             return 'not over'
-    for j in range(len(mat)-1): #check up/down entries on last column
-        if mat[j][len(mat)-1]==mat[j+1][len(mat)-1]:
+    # check up/down entries on last column
+    for j in range(len(mat) - 1):
+        if mat[j][len(mat) - 1] == mat[j + 1][len(mat) - 1]:
             return 'not over'
     return 'lose'
 
-###########
-# Task 2a #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
 
 def reverse(mat):
-    new=[]
+    new = []
     for i in range(len(mat)):
         new.append([])
         for j in range(len(mat[0])):
-            new[i].append(mat[i][len(mat[0])-j-1])
+            new[i].append(mat[i][len(mat[0]) - j - 1])
     return new
 
-###########
-# Task 2b #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
 
 def transpose(mat):
-    new=[]
+    new = []
     for i in range(len(mat[0])):
         new.append([])
         for j in range(len(mat)):
             new[i].append(mat[j][i])
     return new
 
-##########
-# Task 3 #
-##########
 
-# [Marking Scheme]
-# Points to note:
 # The way to do movement is compress -> merge -> compress again
 # Basically if they can solve one side, and use transpose and reverse correctly they should
 # be able to solve the entire thing just by flipping the matrix around
 # No idea how to grade this one at the moment. I have it pegged to 8 (which gives you like,
 # 2 per up/down/left/right?) But if you get one correct likely to get all correct so...
 # Check the down one. Reverse/transpose if ordered wrongly will give you wrong result.
-
 def cover_up(mat):
-    new=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    done=False
+    new = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    done = False
     for i in range(4):
-        count=0
+        count = 0
         for j in range(4):
-            if mat[i][j]!=0:
-                new[i][count]=mat[i][j]
-                if j!=count:
-                    done=True
-                count+=1
-    return (new,done)
+            if mat[i][j] != 0:
+                new[i][count] = mat[i][j]
+                if j != count:
+                    done = True
+                count += 1
+    return new, done
+
 
 def merge(mat):
-    done=False
+    done = False
     for i in range(4):
-         for j in range(3):
-             if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
-                 mat[i][j]*=2
-                 mat[i][j+1]=0
-                 done=True
-    return (mat,done)
+        for j in range(3):
+            if mat[i][j] == mat[i][j + 1] and mat[i][j] != 0:
+                mat[i][j] *= 2
+                mat[i][j + 1] = 0
+                done = True
+    return mat, done
 
 
 def up(game):
-        # return matrix after shifting up
-        game=transpose(game)
-        game,done=cover_up(game)
-        temp=merge(game)
-        game=temp[0]
-        done=done or temp[1]
-        game=cover_up(game)[0]
-        game=transpose(game)
-        return (game,done)
+    # return matrix after shifting up
+    game = transpose(game)
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = transpose(game)
+    return game, done
+
 
 def down(game):
-        game=reverse(transpose(game))
-        game,done=cover_up(game)
-        temp=merge(game)
-        game=temp[0]
-        done=done or temp[1]
-        game=cover_up(game)[0]
-        game=transpose(reverse(game))
-        return (game,done)
+    game = reverse(transpose(game))
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = transpose(reverse(game))
+    return game, done
+
 
 def left(game):
-        # return matrix after shifting left
-        game,done=cover_up(game)
-        temp=merge(game)
-        game=temp[0]
-        done=done or temp[1]
-        game=cover_up(game)[0]
-        return (game,done)
+    # return matrix after shifting left
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    return game, done
+
 
 def right(game):
-        # return matrix after shifting right
-        game=reverse(game)
-        game,done=cover_up(game)
-        temp=merge(game)
-        game=temp[0]
-        done=done or temp[1]
-        game=cover_up(game)[0]
-        game=reverse(game)
-        return (game,done)
-def makeMove(game,move):
+    # return matrix after shifting right
+    game = reverse(game)
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = reverse(game)
+    return game, done
+
+
+def makeMove(game, move):
     if move in "'w'":
-       return up(game)
+        return up(game)
     if move in "'a'":
-       return left(game)
+        return left(game)
     if move in "'s'":
-       return down(game)
+        return down(game)
     if move in "'d'":
-       return right(game)
+        return right(game)
--- Game/puzzle.py
+++ Game/puzzle.py
@@ -1,11 +1,13 @@
-from Tkinter import *
-from logic import *
-from alphaBetaAi import *
+import tkinter as tk
 import random as ran
-from datetime import datetime
-from Minimax import *
-from expectiMax import *
 import time
+import json
+from datetime import datetime
+
+from Game.logic import *
+from Game.alphaBetaAi import *
+from Game.Minimax import *
+from Game.expectiMax import *
 
 SIZE = 500
 GRID_LEN = 4
@@ -13,12 +15,32 @@ GRID_PADDING = 10
 
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
-BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", \
-                         32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 256: "#edcc61", \
-                         512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
-CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2", \
-                   32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2", 256: "#f9f6f2", \
-                   512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2"}
+BACKGROUND_COLOR_DICT = {
+    2: "#eee4da",
+    4: "#ede0c8",
+    8: "#f2b179",
+    16: "#f59563",
+    32: "#f67c5f",
+    64: "#f65e3b",
+    128: "#edcf72",
+    256: "#edcc61",
+    512: "#edc850",
+    1024: "#edc53f",
+    2048: "#edc22e"
+}
+CELL_COLOR_DICT = {
+    2: "#776e65",
+    4: "#776e65",
+    8: "#f9f6f2",
+    16: "#f9f6f2",
+    32: "#f9f6f2",
+    64: "#f9f6f2",
+    128: "#f9f6f2",
+    256: "#f9f6f2",
+    512: "#f9f6f2",
+    1024: "#f9f6f2",
+    2048: "#f9f6f2"
+}
 FONT = ("Verdana", 40, "bold")
 
 KEY_UP_ALT = "\'\\uf700\'"
@@ -26,26 +48,41 @@ KEY_DOWN_ALT = "\'\\uf701\'"
 KEY_LEFT_ALT = "\'\\uf702\'"
 KEY_RIGHT_ALT = "\'\\uf703\'"
 
-
 KEY_UP = "'w'"
 KEY_DOWN = "'s'"
 KEY_LEFT = "'a'"
 KEY_RIGHT = "'d'"
 
+data = {
+    "timeTaken": 0,
+    "startTime": 0,
+    "score": 0,
+    "moves": 0,
+    "maxTile": 0,
+    "win?": 0,
+    "statesScanned": 0
+}
 
-data = {"timeTaken":0,"startTime":0,"score":0,"moves":0,"maxTile":0,"win?":0,"statesScanned":0}
 
-class GameGrid(Frame):
-    def __init__(self,times,type):
-        Frame.__init__(self)
+class GameGrid(tk.Frame):
+    def __init__(self, times, type):
+        tk.Frame.__init__(self)
 
         self.grid()
         self.master.title('2048')
         self.master.bind("<Key>", self.key_down)
         self.type = type
         # self.gamelogic = gamelogic
-        self.commands = {KEY_UP: up, KEY_DOWN: down, KEY_LEFT: left, KEY_RIGHT: right,
-                         KEY_UP_ALT: up, KEY_DOWN_ALT: down, KEY_LEFT_ALT: left, KEY_RIGHT_ALT: right}
+        self.commands = {
+            KEY_UP: up,
+            KEY_DOWN: down,
+            KEY_LEFT: left,
+            KEY_RIGHT: right,
+            KEY_UP_ALT: up,
+            KEY_DOWN_ALT: down,
+            KEY_LEFT_ALT: left,
+            KEY_RIGHT_ALT: right
+        }
 
         self.grid_cells = []
         self.init_grid()
@@ -55,23 +92,38 @@ class GameGrid(Frame):
         self.times = times
         self.playTimes()
         self.mainloop()
+
     def init_grid(self):
-        background = Frame(self, bg=BACKGROUND_COLOR_GAME, width=SIZE, height=SIZE)
+        background = tk.Frame(
+            self, bg=BACKGROUND_COLOR_GAME, width=SIZE, height=SIZE)
         background.grid()
         for i in range(GRID_LEN):
             grid_row = []
             for j in range(GRID_LEN):
-                cell = Frame(background, bg=BACKGROUND_COLOR_CELL_EMPTY, width=SIZE / GRID_LEN, height=SIZE / GRID_LEN)
-                cell.grid(row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
-                # font = Font(size=FONT_SIZE, family=FONT_FAMILY, weight=FONT_WEIGHT)
-                t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY, justify=CENTER, font=FONT, width=4,
-                          height=2)
+                cell = tk.Frame(
+                    background,
+                    bg=BACKGROUND_COLOR_CELL_EMPTY,
+                    width=SIZE / GRID_LEN,
+                    height=SIZE / GRID_LEN)
+                cell.grid(
+                    row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
+                # font = tk.Font(size=FONT_SIZE, family=FONT_FAMILY, weight=FONT_WEIGHT)
+                t = tk.Label(
+                    master=cell,
+                    text="",
+                    bg=BACKGROUND_COLOR_CELL_EMPTY,
+                    justify=tk.CENTER,
+                    font=FONT,
+                    width=4,
+                    height=2)
                 t.grid()
                 grid_row.append(t)
 
             self.grid_cells.append(grid_row)
+
     def gen(self):
         return randint(0, GRID_LEN - 1)
+
     def init_matrix(self):
         self.matrix = new_game(4)
         self.time = datetime.now()
@@ -83,10 +135,13 @@ class GameGrid(Frame):
             for j in range(GRID_LEN):
                 new_number = self.matrix[i][j]
                 if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[i][j].configure(
+                        text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
                 else:
-                    self.grid_cells[i][j].configure(text=str(new_number), bg=BACKGROUND_COLOR_DICT[new_number],
-                                                    fg=CELL_COLOR_DICT[new_number])
+                    self.grid_cells[i][j].configure(
+                        text=str(new_number),
+                        bg=BACKGROUND_COLOR_DICT[new_number],
+                        fg=CELL_COLOR_DICT[new_number])
         self.update_idletasks()
 
     def key_down(self, event):
@@ -107,29 +162,37 @@ class GameGrid(Frame):
             self.makeMove(key)
             self.gameOver()
 
-#checks wether the game is won or lost and updates grid to display state
+
+# checks wether the game is won or lost and updates grid to display state
+
     def gameOver(self):
         if game_state(self.matrix) == 'win':
-            self.grid_cells[1][1].configure(text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
-            self.grid_cells[1][2].configure(text="Win!", bg=BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][1].configure(
+                text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][2].configure(
+                text="Win!", bg=BACKGROUND_COLOR_CELL_EMPTY)
             self.update()
             time.sleep(1)
             return True
         if game_state(self.matrix) == 'lose':
-            self.grid_cells[1][1].configure(text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
-            self.grid_cells[1][2].configure(text="Lose!", bg=BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][1].configure(
+                text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][2].configure(
+                text="Lose!", bg=BACKGROUND_COLOR_CELL_EMPTY)
             self.update()
             time.sleep(1)
             return True
         return False
+
     def generate_next(self):
         index = (self.gen(), self.gen())
         while self.matrix[index[0]][index[1]] != 0:
             index = (self.gen(), self.gen())
         self.matrix[index[0]][index[1]] = 2
 
-#take input KEY_UP ,KEY_DOWN ,KEY_LEFT,KEY_RIGHT and updates the grid based on move
-    def makeMove(self,move):
+    # take input KEY_UP ,KEY_DOWN ,KEY_LEFT,KEY_RIGHT
+    # and updates the grid based on move
+    def makeMove(self, move):
         self.matrix, done = self.commands[move](self.matrix)
         if done:
             self.matrix = add_two(self.matrix)
@@ -147,7 +210,7 @@ class GameGrid(Frame):
             time = datetime.now() - self.time
             data["timeTaken"] = str(time)
         with open("data.txt", "a") as myfile:
-            myfile.write(str(data)+'\n')
+            myfile.write(str(data) + '\n')
         self.gameOver()
 
     # Alpha-Beta Player
@@ -156,7 +219,7 @@ class GameGrid(Frame):
         data["startTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         while not self.gameOver():
             data["moves"] += 1
-            move=player.getMove(self.matrix)
+            move = player.getMove(self.matrix)
             self.makeMove(move)
         if game_state(self.matrix) == 'win':
             data["win?"] = 1
@@ -166,7 +229,7 @@ class GameGrid(Frame):
         data["timeTaken"] = str(time)
         data["statesScanned"] = alphaBetaPlayer.statesScanned
         with open("abResults.txt", "a") as myfile:
-            myfile.write(str(data)+'\n')
+            myfile.write(str(data) + '\n')
 
     # Minimax Player
     def minimax(self):
@@ -174,7 +237,7 @@ class GameGrid(Frame):
         data["startTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         while not self.gameOver():
             data["moves"] += 1
-            move=player.getMove(self.matrix)
+            move = player.getMove(self.matrix)
             self.makeMove(move)
         if game_state(self.matrix) == 'win':
             data["win?"] = 1
@@ -182,17 +245,17 @@ class GameGrid(Frame):
         data["score"] = score(self.matrix)
         time = datetime.now() - self.time
         data["timeTaken"] = str(time)
-        data["statesScanned"]=minimaxPlayer.statesScanned
+        data["statesScanned"] = minimaxPlayer.statesScanned
         with open("minimaxResults.txt", "a") as myfile:
-            myfile.write(str(data)+'\n')
+            myfile.write(str(data) + '\n')
 
     # Expectimax Player
     def expect(self):
-        player= expectiMaxPlayer()
+        player = expectiMaxPlayer()
         data["startTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         while not self.gameOver():
             data["moves"] += 1
-            move=player.getMove(self.matrix)
+            move = player.getMove(self.matrix)
             self.makeMove(move)
         if game_state(self.matrix) == 'win':
             data["win?"] = 1
@@ -202,7 +265,7 @@ class GameGrid(Frame):
         data["timeTaken"] = str(time)
         data["statesScanned"] = expectiMaxPlayer.statesScanned
         with open("expectimaxResults.txt", "a") as myfile:
-            myfile.write(str(data)+'\n')
+            myfile.write(str(data) + '\n')
 
     def quit(self):
         self.destroy()
@@ -211,22 +274,24 @@ class GameGrid(Frame):
         # print " ", minimaxPlayer.nodesVisited, minimaxPlayer.evalCalls
         # minimaxPlayer.nodesVisited =0
         # minimaxPlayer.evalCalls=0
-        #print "Min ,Alpha ,Expec ", minimaxPlayer.statesScanned,alphaBetaPlayer.statesScanned, expectiMaxPlayer.statesScanned
+        # print "Min ,Alpha ,Expec ", minimaxPlayer.statesScanned,
+        # alphaBetaPlayer.statesScanned, expectiMaxPlayer.statesScanned
         print("game is resetting board")
-        minimaxPlayer.statesScanned=0
+        minimaxPlayer.statesScanned = 0
         alphaBetaPlayer.statesScanned = 0
-        expectiMaxPlayer.statesScanned=0
-        data["timeTaken"]=0
-        data["maxTile"]=0
-        data["startTime"]=0
-        data["score"]=0
-        data["moves"]=0
-        data["win?"]=0
+        expectiMaxPlayer.statesScanned = 0
+        data["timeTaken"] = 0
+        data["maxTile"] = 0
+        data["startTime"] = 0
+        data["score"] = 0
+        data["moves"] = 0
+        data["win?"] = 0
         self.init_matrix()
         self.update_grid_cells()
+
     def playTimes(self):
 
-        while self.times >0:
+        while self.times > 0:
             if self.type in "'r'":
                 self.randomPlayer()
 
@@ -242,11 +307,12 @@ class GameGrid(Frame):
             if self.gameOver():
                 self.times -= 1
                 self.resetBoard()
-        if self.times ==0:
+        if self.times == 0:
             quit()
 
-algotype="'e'"         #change to either 'b' or 'm' or 'e'
-gamegrid=GameGrid(100,algotype)   #change to number of rounds
+algotype = "'e'"  # change to either 'b' or 'm' or 'e'
+gamegrid = GameGrid(100, algotype)  # change to number of rounds
+
 
 def quit():
     gamegrid.destroy()
